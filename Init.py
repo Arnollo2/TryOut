b35291c7a064a0733b8313ed7174a540edce2797
Arnollo2 <arnewehling@gmail.com>
2020-11-15 21:10:30 +0100

    Wavelength Quant Added

--- Init.py
+++ Init.py
@@ -12,7 +12,7 @@ import xml.etree.ElementTree as ET #needed to read in XML
 
 class qTLExperiment:
     
-    def __init__(self,Directory, MovieName, SegmentationMethod = 'Overlay', Wavelength_Segment = ['BF1', 'BF2'], Wavelength_Quant = ['PE','NIR','Al488']):
+    def __init__(self,Directory, MovieName, SegmentationMethod = 'Overlay', Wavelength_Segment = ['BF1', 'BF2'], Wavelength_Quant = []):
         self.Directory          = Directory
         self.MovieName          = MovieName
         self.Segmentation       = SegmentationMethod
@@ -61,25 +61,39 @@ class qTLExperiment:
             self.WavelengthComment  = [wl.attrib['Comment'] for wl in root.findall('./WavelengthData/WavelengthInformation/')]
             self.WavelengthSuffix   = [('w00'[0:len('w00')-len(str(wl))] + str(wl)) for wl in range(0,self.WavelengthCount)]      
             
-        ## Identify which WL should be used for Segmentation, Check if Comment Or Suffix was provided by User, also convert to lower case
+        ## Identify which WL should be used for Segmentation
+        #Check if Comment Or Suffix was provided by User, also convert to lower case
         SegmentSuffix   =  [check.lower() for check in self.Wavelength_Segment if check.lower() in self.WavelengthSuffix] #provided as Wavelength file name suffix
         SegmentComment  =  [check for check in self.Wavelength_Segment if check in self.WavelengthComment] #provided as Channel naming comment in Youscope
         
-        
-        #NOT WORKING YET!!!!! DOES NOT RECOGNIZE SegmentationChanel input
         if SegmentSuffix: #False if empty
             self.WavelengthSuffix_Segment = SegmentSuffix #already in right format: Wavelength file suffix
         elif SegmentComment: #False if empty
-            SegmentWL_Comment_Idx= [i for i in range(0,len(self.WavelengthComment)) if self.WavelengthComment[i] in self.Wavelength_Segment]
-            self.WavelengthSuffix_Segment = self.WavelengthSuffix[SegmentWL_Comment_Idx]
+            Comment_Idx= [i for i in range(0,len(self.WavelengthComment)) if self.WavelengthComment[i] in self.Wavelength_Segment]
+            self.WavelengthSuffix_Segment = [self.WavelengthSuffix[i] for i in Comment_Idx]
         else:
-            print('Error: Please Provide at least one available Wavelength in Movie for Segmentation with variable \'SegmentationChannel\'! ' +
-                  'Wavelength available: ' + ', '.join(self.WavelengthComment))
+            print('Error: Please Provide at least one available Wavelength in Movie for Segmentation with variable \'Wavelength_Segment\'! ' +
+                  'Wavelengths available: ' + ', '.join(self.WavelengthComment))
+        
+        ## Identify which WL should be used for Quant
+        #Check if Comment Or Suffix was provided by User, also convert to lower case
+        QuantSuffix   =  [check.lower() for check in self.Wavelength_Quant if check.lower() in self.WavelengthSuffix] #provided as Wavelength file name suffix
+        QuantComment  =  [check for check in self.Wavelength_Quant if check in self.WavelengthComment] #provided as Channel naming comment in Youscope
+        
+        if QuantSuffix: #False if empty
+            self.WavelengthSuffix_Quant = QuantSuffix #already in right format: Wavelength file suffix
+        elif QuantComment: #False if empty
+            Comment_Idx= [i for i in range(0,len(self.WavelengthComment)) if self.WavelengthComment[i] in self.Wavelength_Quant]
+            self.WavelengthSuffix_Quant = [self.WavelengthSuffix[i] for i in Comment_Idx]
+        else:
+            print('Error: Please Provide at least one available Wavelength in Movie for Quantification with variable \'Wavelength_Quant\'! ' +
+                  'Wavelengths available: ' + ', '.join(self.WavelengthComment))
+
 
 #ExperimentDir=filedialog.askdirectory(title="########### PLEASE SELECT Working Directory ###########") #Should contain MetaData File
 MovieName   = '200708AW11_16bit'
 MovieDir    = 'T:/TimelapseData/16bit/AW_donotdelete_16bit/200708AW11_16bit/'
-Mov         = qTLExperiment(MovieDir, MovieName,   Wavelength_Segment= 'BF')
+Mov         = qTLExperiment(MovieDir, MovieName,   Wavelength_Segment= 'BF',Wavelength_Quant= ['Rab11-Al488', 'Itgb4-PE','LysobriteNIR'])
 
 Mov.CheckType()
 Mov.ReadInMetaData()
