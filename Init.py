8267dc79482b26fb1d8f8afa0bdaf8c60bb521cb
Arnollo2 <arnewehling@gmail.com>
2020-11-16 23:38:41 +0100

    Can create Iterable now, update it and track progress

--- Init.py
+++ Init.py
@@ -50,12 +50,14 @@ class qTLExperiment:
         else:                                                                    #### Parse TATexp.XML ####                                                                   
             print('TATexp.XMLfound --> Parsing')
             self.OutputDirectory    = self.Directory + '/Analysis/OnlineMovieAnalysis/'
-            try:
-                os.mkdir(self.OutputDirectory)
-            except OSError:
-                print ("Creation of the directory %s failed" % self.OutputDirectory)
-            else:
-                print ("Successfully created the directory %s " % self.OutputDirectory)
+            
+            if not os.path.isdir(self.OutputDirectory):
+                try:
+                    os.mkdir(self.OutputDirectory)
+                except OSError:
+                    print ("Creation of the directory %s failed" % self.OutputDirectory)
+                else:
+                    print ("Successfully created the directory %s " % self.OutputDirectory)
             
             self.WavelengthSuffix_standard   = 'w00'
             self.TimeSuffix_standard         = 't00000'
@@ -71,13 +73,11 @@ class qTLExperiment:
             self.PositionName       = [pos.attrib['comments'].split(' [')[0] for pos in root.findall('./PositionData/PositionInformation/')] 
             self.PositionIndex      = [int(pos.attrib['index']) for pos in root.findall('./PositionData/PositionInformation/')] 
             self.PositionSuffix     = [(self.PositionSuffix_standard[0:len(self.PositionSuffix_standard)-len(str(pos))] + str(pos)) for pos in self.PositionIndex]
+            
             PositionDirs            = os.listdir(self.Directory)
-            self.PositionDirs       = [(self.Directory + pos) for pos in PositionDirs if sum((possuffix in pos) for possuffix in self.PositionSuffix)==1]
-            #self.PositionDirs       = [PositionDirs[idx-1] for idx in self.PositionIndex]
-            
-            
+            self.PositionDirs       = [(self.Directory + pos ) for pos in PositionDirs if  os.path.isdir(self.Directory + pos) & (sum((possuffix in pos) for possuffix in self.PositionSuffix)==1)] #only list psotions if dir and contains specified position suffix
+
             self.lastTPchecked      = [0 for i in self.PositionIndex] #to check last iterated TP per Position
-            
             self.WavelengthCount    = int(root.find('./WavelengthCount').attrib['count']) 
             self.WavelengthComment  = [wl.attrib['Comment'] for wl in root.findall('./WavelengthData/WavelengthInformation/')]
             self.WavelengthSuffix   = [(self.WavelengthSuffix_standard[0:len(self.WavelengthSuffix_standard)-len(str(wl))] + str(wl)) for wl in range(0,self.WavelengthCount)]      
@@ -108,32 +108,77 @@ class qTLExperiment:
                   'Wavelengths available: ' + ', '.join(self.WavelengthComment))
          
             
-    def CreateIteratable(self): 
+    def CreateIterable(self): 
         #Get Segmentation Images per position 
         #This will create a List with image names, or if multiple images found per position a list of lists, with a list for every position
-        self.Position2beIterated = [glob.glob(pos + '/*' + self.WavelengthSuffix_Segment[0] + '*' + self.ImageFormat) for pos in self.PositionDirs]
+        #self.Position2beIterated = [glob.glob(pos + '/*' + self.WavelengthSuffix_Segment[0] + '*' + self.ImageFormat) for pos in self.PositionDirs]
+        self.Position2beIterated = [os.listdir(pos) for pos in self.PositionDirs]
+        for i in range(0,len(self.Position2beIterated)):
+            #only check for images to be segmented
+            self.Position2beIterated[i] = [img for img in self.Position2beIterated[i] if (self.WavelengthSuffix_Segment[0] in img) & (self.ImageFormat in img) ]
+           
+    def TrackIterable(self, currentPosition_Index,currentTP):
+        #currentTP will be parsed from image name
+        #Register the lastTPChecked per Position and clear the respective Position2beIterated entry
+        PositionPointer                             = [i for i in range(0,self.PositionCount) if self.PositionIndex[i] == currentPosition_Index]
+        PositionPointer                             = PositionPointer[0]#still list get entry alone
+        self.lastTPchecked[PositionPointer]         = currentTP
         
+    def UpdateIterable(self):
+        #Check images per Position 
+        self.CreateIterable()
+        #Remove all Images before and including latest lastTPchecked entry
+        for i in range(0,len(self.Position2beIterated)):
+            FilesFoundInPosition        = self.Position2beIterated[i]
+            TPs2Exlcude                 = [(self.TimeSuffix_standard[0:len(self.TimeSuffix_standard)-len(str(tp))] + str(tp)) for tp in range(0,self.lastTPchecked[i]+1)]
+            NewFilesFoundInPosition     = [img for img in FilesFoundInPosition if sum([(fi in img) for fi in TPs2Exlcude]) == 0] #only get images that do not belong to timepoints already seen.
+            self.Position2beIterated[i] = NewFilesFoundInPosition
 
-    def CreateIterator(self, currentPosition_Index):
-        #given a CurrentPosition_Index
-        if not currentPosition_Index in self.PositionIndex:
-            print('Current position not selected from Positions available. Please chose \'currentPosition\' contained in \'PositionIndex\'!')
-        else:
-            #Create Dictionary that contains types, entries and names of internal variables ppased onto the FieldOfView Object
-            print('currently at position ' + str(currentPosition_Index))
-            keys = ['Directory','MovieName','Segmentation','WavelengthSuffix_Quant','WavelengthSuffix_Segment','ImageFormat']
 
-        
+'''
+def CreateIterator(self, currentPosition_Index):
+    #given a CurrentPosition_Index
+    if not currentPosition_Index in self.PositionIndex:
+        print('Current position not selected from Positions available. Please chose \'currentPosition\' contained in \'PositionIndex\'!')
+    else:
+        #Create Dictionary that contains types, entries and names of internal variables ppased onto the FieldOfView Object
+        print('currently at position ' + str(currentPosition_Index))
+
+        PositionPointer = [i for i in range(0,self.PositionCount) if self.PositionIndex[i] == currentPosition_Index]
+        keys   = [lastTP]
+        Values = getattr(Mov)
+        return(PositionPointer, keys)
+ '''       
 #ExperimentDir=filedialog.askdirectory(title="########### PLEASE SELECT Working Directory ###########") #Should contain MetaData File
+
+
+#Test
 MovieName   = '200708AW11_16bit'
 MovieDir    = 'T:/TimelapseData/16bit/AW_donotdelete_16bit/200708AW11_16bit/'
 Mov         = qTLExperiment(MovieDir, MovieName, ImageFormat='.png',  Wavelength_Segment= 'BF',Wavelength_Quant= ['Rab11-Al488', 'Itgb4-PE','LysobriteNIR'])
-qTLExperiment
+
+
 Mov.CheckType()
 Mov.ReadInMetaData()
 
+#Check for available images per position
 start_time = time.time()
-Mov.CreateIteratable()
+Mov.CreateIterable()
 print("--- %s seconds ---" % (time.time() - start_time))
 
-Mov.CreateIterator(currentPosition_Index=91)
+#Should be a full list with exactly one t00001 iamge w00 per entry
+Mov.Position2beIterated
+
+#update every postion as having just seen t00001
+for currentPosition_Index in Mov.PositionIndex:
+    Mov.TrackIterable(currentPosition_Index=currentPosition_Index,currentTP=1)
+    
+#Update Iterable
+start_time = time.time()
+Mov.UpdateIterable()
+print("--- %s seconds ---" % (time.time() - start_time))
+
+#Should be emptie now
+Mov.Position2beIterated
+
+#Works
